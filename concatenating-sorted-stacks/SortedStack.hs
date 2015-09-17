029e034ddcc9aaa29ba68686f4d9b552b4b19d3a
Alvaro Garcia <derecerca@gmail.com>
2015-09-17 21:01:33 +0200

    initial version of the sorted stacks

---
+++ concatenating-sorted-stacks/SortedStack.hs
@@ -0,0 +1,13 @@
+main =putStrLn "hello"
+
+st1 = [0,2,4,6,8]
+st2 = [1,3,5,7,9]
+
+ste = [0,1,2,3,4,5,6,7,8,9]
+
+merg [] [] = []
+merg [] (x:xs) = x : merg [] xs
+merg (x:xs) [] = x : merg xs []
+merg (x:xs) (x':xs') = case x `compare` x' of
+    GT -> x': (x  : (merg xs xs'))
+    _  -> x : (x' : (merg xs xs'))
