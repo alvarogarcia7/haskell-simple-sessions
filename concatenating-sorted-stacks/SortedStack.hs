5ca6750c78f3a3099177dbc764ac2e4371899549
Alvaro Garcia <derecerca@gmail.com>
2015-09-17 22:14:47 +0200

    add acceptance test

--- concatenating-sorted-stacks/SortedStack.hs
+++ concatenating-sorted-stacks/SortedStack.hs
@@ -24,11 +24,9 @@ main = do
             merg [2,3] [1] `shouldBe` [1,2,3]
             merg [2,3] [1,3,3,3,4] `shouldBe` [1,2,3,3,3,3,4]
 
-
-st1 = [0,2,4,6,8]
-st2 = [1,3,5,7,9]
-
-ste = [0,1,2,3,4,5,6,7,8,9]
+        describe "acceptance test" $ do
+          it "should sort stacks" $ do
+            merg [0,2,4,6,8] [1,3,5,7,9] `shouldBe` [0,1,2,3,4,5,6,7,8,9]
 
 merg :: Ord a => [a] -> [a] -> [a]
 merg [] [] = []
