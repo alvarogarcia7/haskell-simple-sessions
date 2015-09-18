6030585800a5fec7c21334f45b590aa346def41e
Alvaro Garcia <derecerca@gmail.com>
2015-09-18 17:54:48 +0200

    green: permutation for repeated elements

--- permutations/Permutations.hs
+++ permutations/Permutations.hs
@@ -14,8 +14,12 @@ main = hspec $ do
       it "of a 1-element array should be unique" $ do
         (permutate [1]) `shouldBe` ([[1]] :: [[Int]])
 
-      it "of a 2-element array" $ do
-        (permutate [1,2]) `shouldBe` ([[1,2], [2,1]] :: [[Int]])
+      describe "of a 2-element array" $ do
+      	it "no repeated elements" $ do
+          (permutate [1,2]) `shouldBe` ([[1,2], [2,1]] :: [[Int]])
+
+        it "with repeated elements" $ do
+          (permutate [3,3]) `shouldBe` ([[3,3], [3,3]] :: [[Int]]) --because permutations are performed on sets, therefore the two threes (3) are different
 
 permutate :: [a] -> [[a]]
 permutate [] = []
