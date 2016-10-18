e56a0036f4ca981d76d9028eb5cf8cd995a2b956
Alvaro Garcia <derecerca@gmail.com>
2016-10-18 23:32:54 +0100

    calculate prices for series of books

--- series-of-books/main.hs
+++ series-of-books/main.hs
@@ -19,3 +19,15 @@ length'' (x:xs) = 1 + length'' xs
 flatMap :: (a -> [b]) -> [a] -> [b]
 flatMap f xs = foldl (\acc x -> (acc ++ f x)) [] xs
 
+priceFor :: [[b]] -> Double
+priceFor cart = foldl (+) 0.0 (map priceForSerie cart)
+
+priceForSerie :: [b] -> Double
+priceForSerie serie = serieLength * 8.0 * (1.0 - discount) where
+    discount = (fromIntegral ((length serie) - 1)::Double) * 0.05
+    serieLength = fromIntegral ((length serie))::Double
+
+--
+--Main> map (\cart -> (cart, priceFor cart)) (cartsFor [1,2])
+--[([[2],[1],[]],16.0),([[2],[1]],16.0),([[2,1]],15.2),([[2],[1]],16.0),([[2,1]],15.2)]
+
