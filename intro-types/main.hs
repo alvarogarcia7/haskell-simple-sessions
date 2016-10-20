a3128857a65a5ac8705d4d5dc7ef3fce6848dd37
Alvaro Garcia <derecerca@gmail.com>
2016-10-20 01:44:21 +0100

    Green: a cart has a length

--- intro-types/main.hs
+++ intro-types/main.hs
@@ -15,6 +15,14 @@ serieFor = id
 cart :: Serie s -> Cart s
 cart s = [s]
 
+length' :: Cart c -> Int
+length' [serie] = length'' serie
+length' (serie:restOfCart) = length'' serie + length' restOfCart
+
+length'' :: Serie s -> Int
+length'' [] = 0
+length'' (book:restOfSerie) = 1 + length'' restOfSerie
+
 main = hspec $ do
     describe "a simple type" $ do
         it "is equivalent to its primitive representation" $ do
@@ -24,4 +32,11 @@ main = hspec $ do
 
         it "can be manipulated as a list (underlying representation)" $ do
             (1:(serie 2)) `shouldBe` [1,2];
-            
+
+
+    describe "a cart" $ do
+        it "has a length" $ do
+            (length' (cart [1])) `shouldBe` 1;
+            (length' ([2]:cart [1])) `shouldBe` 2;
+            (length' ([2,3]:cart [1])) `shouldBe` 3;
+            (length' ([3,4]:([2,3]:cart [1]))) `shouldBe` 5;
