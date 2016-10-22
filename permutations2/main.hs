a7054040027c1a527bac2685c581f287afdad1d2
Alvaro Garcia <derecerca@gmail.com>
2016-10-22 08:06:00 +0100

    green

--- permutations2/main.hs
+++ permutations2/main.hs
@@ -11,6 +11,24 @@ main = hspec $ do
         it "for a one-element list" $
             (permutations [1] :: [[Int]]) `shouldBe` ([[1]] :: [[Int]])
 
-        it "for a multiple-element list" $
+        it "for a multiple-element list" $ do
             (permutations [1,2] :: [[Int]]) `shouldBe` ([[1,2], [2,1]] :: [[Int]]);
             length (permutations [1,2,3] :: [[Int]]) `shouldBe` 6;
+
+
+permutations :: [a] -> [[a]]
+permutations xs = permutations' [] xs [[]] where
+    permutations' _ [] acc = acc
+    permutations' used (head:rest) acc = permutations' (head:used) rest x where
+            x = aNew acc head where
+                aNew [[]] b = [[b]]
+                aNew acc ele = flatMap (\perm -> map (\pos -> setAt perm pos ele) (reverse [0..(length perm)])) acc
+
+flatMap f xs = foldl (\ acc ele -> acc++f ele) [] xs
+
+setAt :: [a] -> Int -> a -> [a]
+setAt [] 0 ele = [ele]
+setAt [] _ _ = []
+setAt coll 0 ele = ele:coll
+setAt (x:xs) n ele = x:(setAt xs (n-1) ele)
+
