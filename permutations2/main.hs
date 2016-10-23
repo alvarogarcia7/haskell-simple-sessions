d25c7fc7397cf1a3b832ebbba3791a08a0f3c33e
Alvaro Garcia <derecerca@gmail.com>
2016-10-23 08:08:00 +0100

    Refactor: add more complex case

--- permutations2/main.hs
+++ permutations2/main.hs
@@ -5,15 +5,18 @@ import Text.Printf (printf)
 
 main = hspec $ do
     describe "generates permutations" $ do
-        it "for the empty list" $
+        it "for the empty list" $ do
             (permutations [] :: [[Int]]) `shouldBe` ([[]] :: [[Int]])
 
-        it "for a one-element list" $
+        it "for a one-element list" $ do
             (permutations [1] :: [[Int]]) `shouldBe` ([[1]] :: [[Int]])
 
         it "for a multiple-element list" $ do
             (permutations [1,2] :: [[Int]]) `shouldBe` ([[1,2], [2,1]] :: [[Int]]);
-            length (permutations [1,2,3] :: [[Int]]) `shouldBe` 6;
+        
+        it "for a multiple-element list" $ do
+            (permutations [1,2,3] :: [[Int]]) `shouldBe` ([[1,2,3],[1,3,2],[3,1,2],[2,1,3],[2,3,1],[3,2,1]]::[[Int]]);
+            (length (permutations [1,2,3] :: [[Int]])) `shouldBe` (6::Int);
 
 
 permutations :: [a] -> [[a]]
