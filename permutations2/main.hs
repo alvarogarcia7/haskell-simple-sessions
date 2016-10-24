b96fb0b9eb35c81d613735abf587982448a25c49
Alvaro Garcia <derecerca@gmail.com>
2016-10-24 07:30:46 +0100

    Refactor: swap assertions to make it easier to debug

--- permutations2/main.hs
+++ permutations2/main.hs
@@ -15,8 +15,8 @@ main = hspec $ do
             (permutations [1,2] :: [[Int]]) `shouldBe` ([[1,2], [2,1]] :: [[Int]]);
         
         it "for a multiple-element list" $ do
-            (permutations [1,2,3] :: [[Int]]) `shouldBe` ([[1,2,3],[1,3,2],[3,1,2],[2,1,3],[2,3,1],[3,2,1]]::[[Int]]);
             (length (permutations [1,2,3] :: [[Int]])) `shouldBe` (6::Int);
+            (permutations [1,2,3] :: [[Int]]) `shouldBe` ([[1,2,3],[1,3,2],[3,1,2],[2,1,3],[2,3,1],[3,2,1]]::[[Int]]);
 
 
 permutations :: [a] -> [[a]]
