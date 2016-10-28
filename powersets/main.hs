487d6eb52c7289bf79ec317f3d5484b96225b129
Alvaro Garcia <derecerca@gmail.com>
2016-10-28 09:07:40 +0100

    Green: compute powersets (as long as the input is the empty set)

---
+++ powersets/main.hs
@@ -0,0 +1,18 @@
+module PowerSets where
+
+import Test.Hspec
+import Data.List
+import Text.Printf (printf)
+
+powersets :: [x] -> [[x]]
+powersets [] = [[]]
+
+main = hspec $ do
+        describe "canary" $ do
+            it "truth" $ do
+                True `shouldBe` True;
+
+        describe "powersets" $ do
+            it "base case" $ do
+                ((powersets []) :: [[Int]]) `shouldBe` [[]];
+
