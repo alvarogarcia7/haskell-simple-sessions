edb94f8c188d3e0535ec430036d7ead0e9af253b
Alvaro Garcia <derecerca@gmail.com>
2016-11-13 15:59:18 +0000

    Red: unfold with one level and one function only

--- trees/test/HSpecTests.hs
+++ trees/test/HSpecTests.hs
@@ -5,9 +5,9 @@ import Text.Printf (printf)
 
 main = hspec $ do
     describe "unfolds a tree" $ do
-        it "for the empty root" $ do
-            ((unfoldTree 0 [id] 1) :: [[Int]]) `shouldBe` ([[],[],[]] :: [[Int]])
-
         it "for the empty array of unfolding functions" $ do
             ((unfoldTree 0 [] 1) :: [[Int]]) `shouldBe` ([] :: [[Int]])
+
+        it "with one level only" $ do
+            ((unfoldTree 0 [id] 1) :: [[Int]]) `shouldBe` ([[0],[0],[0]] :: [[Int]])
 
