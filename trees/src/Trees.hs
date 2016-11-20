4fcc2b88d2eb0d3659d199a1ed29a6b61f73bba8
Alvaro Garcia <derecerca@gmail.com>
2016-11-20 19:47:30 +0000

    Refactor: extract explaining variable

--- trees/src/Trees.hs
+++ trees/src/Trees.hs
@@ -11,10 +11,12 @@ depth (Root root children) = 1 + foldl1 max (map depth children)
 buildTree :: a -> [Tree a] -> Tree a
 buildTree root children = Root root children
 
+leaf :: a -> Tree a
+leaf root = Root root []
 
 unfoldTree :: a -> [a -> a] -> Int -> Tree a
 unfoldTree root [] depth = Root root []
-unfoldTree root fns 0 = Root root []
+unfoldTree root fns 0 = leaf root
 unfoldTree root fns n = Root root childrenTrees where
     childrenTrees = map (\child -> unfoldTree child fns (n-1)) children
     children = map (\fn -> fn root) fns
