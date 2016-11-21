3296edd4eea01e0042b26e26dd1ddfa2ffa05d97
Alvaro Garcia <derecerca@gmail.com>
2016-11-21 17:37:28 +0000

    Red: calculate Pascal at level 3

--- trees/test/HSpecTests.hs
+++ trees/test/HSpecTests.hs
@@ -29,6 +29,9 @@ main = hspec $ do
         it "calculates the second level" $ do
             map rootOf (childrenOf (pascal 2)) `shouldBe` [1,2,1]
 
+        it "calculates the third level" $ do
+            (childrenOf (pascal 3)) `shouldBe` [Root 1 [leaf 1, leaf 3], Root 2 [leaf 3], Root 1 [leaf 1]]
+
         describe "helper functions - pair" $ do
             it "calculates pairs in order" $ do
                 (pair $ (Root 1 [leaf 2, leaf 3, leaf 4])) `shouldBe` [[Root 2 [],Root 3 []],[Root 3 [],Root 4 []]]
