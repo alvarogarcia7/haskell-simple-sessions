d0e9f1f617d8f1bc0adc95188adb8881cbe02cd6
Alvaro Garcia <derecerca@gmail.com>
2016-11-22 22:55:16 +0000

    green

--- parser/src/Parser.hs
+++ parser/src/Parser.hs
@@ -1,5 +1,8 @@
 module Parser where
 
+import Data.Text as T (pack, splitOn, unpack)
+
 calculate "T" = True
 calculate "F" = False
 calculate ('N':'O':'T':' ':rest) = not $ calculate rest
+calculate expression = foldl1 (&&) $ map calculate $ map T.unpack $ T.splitOn (T.pack " AND ") (T.pack expression)
