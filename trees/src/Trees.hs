10b403209183b655474a93f28b458f06c60ff4cc
Alvaro Garcia <derecerca@gmail.com>
2016-11-22 09:07:00 +0000

    Add defect

--- trees/src/Trees.hs
+++ trees/src/Trees.hs
@@ -32,6 +32,7 @@ pascal :: Int -> Tree Integer
 pascal 1 = leaf 1
 pascal 2 = appendChildren (pascal 1) [leaf 1, leaf 2, leaf 1]
 -- TODO Based on the test, the tree must be returned in a well-formed fashion. Now the elements hang from any leaf
+-- TODO Defect: Only the elements at level 3 are returned, not the full triangle
 pascal 3 = appendChildren pascal2 (unfoldPairs pascal2 (leaf 1) (\[l,r] -> leaf ((rootOf l) + (rootOf r)))) where
     pascal2 = pascal 2
     unfoldPairs tree boundary expansionFunction = boundary : expansion ++ [boundary] where
