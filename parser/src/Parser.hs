d547697cae0f15bc97fad4342fc32e326cd745b9
Alvaro Garcia <derecerca@gmail.com>
2016-11-22 23:01:04 +0000

    Refactor: extract explaining variables

--- parser/src/Parser.hs
+++ parser/src/Parser.hs
@@ -5,4 +5,7 @@ import Data.Text as T (pack, splitOn, unpack)
 calculate "T" = True
 calculate "F" = False
 calculate ('N':'O':'T':' ':rest) = not $ calculate rest
-calculate expression = foldl1 (&&) $ map calculate $ map T.unpack $ T.splitOn (T.pack " AND ") (T.pack expression)
+calculate expression = do
+    let andDelimiter = T.pack " AND "
+    let expressionParts =  map T.unpack $ T.splitOn andDelimiter (T.pack expression)
+    foldl1 (&&) $ map calculate expressionParts
