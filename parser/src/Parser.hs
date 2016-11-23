7373e28483f5aa0717230883ff976df02f955f3d
Alvaro Garcia <derecerca@gmail.com>
2016-11-23 23:26:16 +0000

    Save WIP: in the process of separating AND and OR - unsucessful

--- parser/src/Parser.hs
+++ parser/src/Parser.hs
@@ -6,6 +6,13 @@ calculate "T" = True
 calculate "F" = False
 calculate ('N':'O':'T':' ':rest) = not $ calculate rest
 calculate expression = do
-    let delimiters = map T.pack [" AND ", " OR "]
-    let expressionParts =  map T.unpack $ foldl (\acc ele -> concat $ map (T.splitOn ele) acc) [T.pack expression] delimiters
-    foldl1 (&&) $ map calculate expressionParts
+    calculate $ calculateWith "AND" (&&) expression
+
+calculateWith str op expression = do
+  let parts = T.splitOn (T.pack " ") $ T.pack expression
+  let whichIndexesAreTheOperator = filter (\i -> not (-1 ==i)) $ zipWith (\text index -> if (text == (T.pack str)) then index else -1) parts [0..]
+  let operand1 = calculate $ T.unpack $ parts !! ((whichIndexesAreTheOperator!!0) -1)
+  let operand2 = calculate $ T.unpack $ parts !! ((whichIndexesAreTheOperator!!0) +1)
+  let rest = snd (splitAt ((whichIndexesAreTheOperator!!0) +2) expression)
+  if (op operand1 operand2) then "T "++rest else "F "++rest
+
