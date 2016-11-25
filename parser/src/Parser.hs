fdd54232343aaea40a7b4ae4b2217a575a084866
Alvaro Garcia <derecerca@gmail.com>
2016-11-25 12:19:47 +0000

    Add type definition for AST

--- parser/src/Parser.hs
+++ parser/src/Parser.hs
@@ -9,3 +9,15 @@ calculate expression = do
     let delimiters = map T.pack [" AND ", " OR "]
     let expressionParts =  map T.unpack $ foldl (\acc ele -> concat $ map (T.splitOn ele) acc) [T.pack expression] delimiters
     foldl1 (&&) $ map calculate expressionParts
+
+data AST op typ = Empty 
+           | Operation op [AST op typ]
+           | Literal typ
+            deriving (Eq)
+
+instance Show (AST a b) where
+    show (Operation _ children) = "function [" ++ (concat $ map show children) ++ "]"
+    show (Literal l) = "literal"
+    show Empty = ""
+
+
