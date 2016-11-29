4f697e30a4b8bc145d17e956652074a758308927
Halima <hkoundi@gmail.com>
2016-11-29 22:29:20 +0000

    WIP: can parse and apply the AND operation without operations inside

--- parser/src/Parser.hs
+++ parser/src/Parser.hs
@@ -11,15 +11,50 @@ calculate expression = do
     foldl1 (&&) $ map calculate expressionParts
 
 data AST op typ = Operation op [AST op typ]
-                | Literal typ
+                | Literale typ
                 deriving (Eq)
 
 instance Show (AST a b) where
     show (Operation _ children) = "function [" ++ (concat $ map show children) ++ "]"
-    show (Literal l) = "literal"
+    show (Literale l) = "literal"
 
 apply :: AST (typ -> typ ->typ) typ -> typ
 apply (Operation fn children) = fn (apply (children !! 0)) (apply (children !! 1))
-apply (Literal l) = l
+apply (Literale l) = l
 
 -- See example-ast.txt
+
+
+--calculate' "T" = "True"
+--calculate' "F" = "False"
+--calculate' "NOT" = "not"
+--calculate' "AND" = "&&"
+--calculate' "OR" = "||"
+calculate' expression = do
+	let delimiters = map T.pack [" "]
+	let expressionParts =  map T.unpack $ foldl (\acc ele -> concat $ map (T.splitOn ele) acc) [T.pack expression] delimiters
+	let parts = map parse expressionParts
+	let fn = case (parts !! 1) of
+		(AND) -> (myAnd)
+	Operation fn [Literale (parts !! 0), Literale (parts !! 2)]
+
+myAnd :: Literale -> Literale -> Literale
+myAnd _ BFalse = BFalse
+myAnd BFalse _ = BFalse
+myAnd BTrue BTrue = BTrue
+
+data Literale = BTrue
+	      | BFalse 
+	      | AND
+
+instance Show Literale where
+	show BTrue = "True"
+	show BFalse = "False"
+	show AND = "&&"
+
+parse :: [Char] -> Literale
+parse b =
+	case b of 
+	 ("T") -> BTrue
+	 ("F") -> BFalse
+	 ("AND") -> AND
