57277fb8c943b848e9b36d1cf63e9a538ac69f38
Alvaro Garcia <derecerca@gmail.com>
2016-12-05 20:06:48 +0000

    Whitespace

--- parser/test/HSpecTests.hs
+++ parser/test/HSpecTests.hs
@@ -4,39 +4,38 @@ import Test.Hspec
 import Text.Printf (printf)
 
 main = hspec $ do
-    describe "canary" $ do
-        it "truthy" $ do
-           True `shouldBe` True
+  describe "canary" $ do
+    it "truthy" $ do
+       True `shouldBe` True
 
-    describe "parse expressions" $ do
-        describe "simple expressions" $ do  
-            it "parses T" $ do
-                calculate "T" `shouldBe` True
+  describe "parse expressions" $ do
+    describe "simple expressions" $ do  
+      it "parses T" $ do
+        calculate "T" `shouldBe` True
 
-            it "parses F" $ do
-                calculate "F" `shouldBe` False
+      it "parses F" $ do
+        calculate "F" `shouldBe` False
 
-        describe "negated expressions" $ do  
-            it "case 1" $ do
-                calculate "NOT T" `shouldBe` False
-                calculate "NOT F" `shouldBe` True
+    describe "negated expressions" $ do  
+      it "case 1" $ do
+        calculate "NOT T" `shouldBe` False
+        calculate "NOT F" `shouldBe` True
 
-        describe "operation AND" $ do  
-            it "case 1" $ do
-                calculate "T AND T" `shouldBe` True
+    describe "operation AND" $ do  
+      it "case 1" $ do
+        calculate "T AND T" `shouldBe` True
 
-            it "supports multiple expressions" $ do
-                calculate "T AND T AND T" `shouldBe` True
-                calculate "T AND T AND F" `shouldBe` False
+      it "supports multiple expressions" $ do
+        calculate "T AND T AND T" `shouldBe` True
+        calculate "T AND T AND F" `shouldBe` False
 
-        describe "operation OR" $ do  
-            it "case 1" $ do
-                calculate "F OR F" `shouldBe` False
+    describe "operation OR" $ do  
+      it "case 1" $ do
+        calculate "F OR F" `shouldBe` False
 
-        describe "combining multiple operations" $ do
-            it "AND and NOT" $ do
-                calculate "T AND NOT F" `shouldBe` True
+    describe "combining multiple operations" $ do
+      it "AND and NOT" $ do
+        calculate "T AND NOT F" `shouldBe` True
 
-            it "NOT and NOT" $ do
-                calculate "NOT NOT F" `shouldBe` False
-
+      it "NOT and NOT" $ do
+        calculate "NOT NOT F" `shouldBe` False
