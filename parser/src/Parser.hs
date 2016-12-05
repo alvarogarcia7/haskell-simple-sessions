bf3552cc47ca8c3fda4dd0ee1e05133c56219409
Alvaro Garcia <derecerca@gmail.com>
2016-12-05 19:51:23 +0000

    Whitespace

--- parser/src/Parser.hs
+++ parser/src/Parser.hs
@@ -8,16 +8,16 @@ calculate "T" = True
 calculate "F" = False
 calculate ('N':'O':'T':' ':rest) = not $ calculate rest
 calculate expression = do
-    let delimiters = map T.pack [" AND ", " OR "]
-    let expressionParts =  map T.unpack $ foldl (\acc ele -> concat $ map (T.splitOn ele) acc) [T.pack expression] delimiters
-    foldl1 (&&) $ map calculate expressionParts
+  let delimiters = map T.pack [" AND ", " OR "]
+  let expressionParts =  map T.unpack $ foldl (\acc ele -> concat $ map (T.splitOn ele) acc) [T.pack expression] delimiters
+  foldl1 (&&) $ map calculate expressionParts
 
 data AST op typ = Operation op [AST op typ]
                 | Literal typ
                 deriving (Eq)
 
 instance Show (AST a b) where
-    show (Operation _ children) = "function [" ++ (concat $ map show children) ++ "]"
+  show (Operation _ children) = "function [" ++ (concat $ map show children) ++ "]"
 
 apply :: AST (typ -> typ ->typ) typ -> typ
 apply (Operation fn children) = fn (apply (children !! 0)) (apply (children !! 1))
