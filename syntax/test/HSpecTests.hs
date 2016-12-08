88fe7222bff060f6fc1aebbdcc1e824334c0bc13
Alvaro Garcia <derecerca@gmail.com>
2016-12-08 08:50:07 +0000

    Refactor: move into a new block

--- syntax/test/HSpecTests.hs
+++ syntax/test/HSpecTests.hs
@@ -12,6 +12,7 @@ main = hspec $ do
         it "binds a value to the expression, making it available in that smaller scope" $ do
            letNotation `shouldBe` 1
 
+    describe "let notation with a do block" $ do
         it "needs a do block to put several expressions together" $ do
            letNotation' `shouldBe` 2
 
