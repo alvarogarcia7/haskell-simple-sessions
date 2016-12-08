ee44fe4cab2f2edbbb3c633f393d3961aae8fa3e
Alvaro Garcia <derecerca@gmail.com>
2016-12-08 08:46:04 +0000

    Green: add test for let/in notation

--- syntax/test/HSpecTests.hs
+++ syntax/test/HSpecTests.hs
@@ -7,3 +7,7 @@ main = hspec $ do
     describe "canary" $ do
         it "truthy" $ do
            True `shouldBe` True
+
+    describe "let/in notation" $ do
+        it "binds a value to the expression, making it available in that smaller scope" $ do
+           letNotation `shouldBe` 1
