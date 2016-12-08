2f4f399379ebd53bdb2ff3be09a69db9b69c5e30
Alvaro Garcia <derecerca@gmail.com>
2016-12-08 08:51:54 +0000

    Refactor: move to another block, whitespace

--- syntax/test/HSpecTests.hs
+++ syntax/test/HSpecTests.hs
@@ -5,18 +5,16 @@ import Text.Printf (printf)
 
 main = hspec $ do
     describe "canary" $ do
-        it "truthy" $ do
-           True `shouldBe` True
+      it "truthy" $ do
+        True `shouldBe` True
 
-    describe "let/in notation" $ do
+    describe "let notation" $ do
+      describe "let/in notation" $ do
         it "binds a value to the expression, making it available in that smaller scope" $ do
-           letNotation `shouldBe` 1
-
-    describe "let notation with a do block" $ do
+          letNotation `shouldBe` 1
+      
+      describe "let notation with a do block" $ do
         it "needs a do block to put several expressions together" $ do
-           letNotation' `shouldBe` 2
-
-
-
+          letNotation' `shouldBe` 2
 
 
