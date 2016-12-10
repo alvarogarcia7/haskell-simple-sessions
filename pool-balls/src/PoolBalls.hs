254bac4e5ea3eed40dc3150c78611cd164266e98
Alvaro Garcia <derecerca@gmail.com>
2016-12-10 18:43:30 +0000

    green

--- pool-balls/src/PoolBalls.hs
+++ pool-balls/src/PoolBalls.hs
@@ -1,5 +1,15 @@
 module PoolBalls where
 
+import Data.Sequence (fromList, update)
+import Data.Foldable (toList)
+
 pairs :: Int -> [[Int]]
 pairs n = [[x,y] | y<-[0..n], x<-[0..y-1]]
 
+apply swap@[from, to] balls = do
+  let balls' = fromList balls
+  let tmp = balls !! from
+  let firstReplaced = update from (balls !! to) $ balls'
+  let secondReplaced = update to tmp firstReplaced
+  toList secondReplaced
+  
