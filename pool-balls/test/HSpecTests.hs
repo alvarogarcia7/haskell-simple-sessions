cd5a24c2975c978a6bfeef608a33e1749a4413f4
Alvaro Garcia <derecerca@gmail.com>
2016-12-10 18:43:21 +0000

    Red: apply a swap

--- pool-balls/test/HSpecTests.hs
+++ pool-balls/test/HSpecTests.hs
@@ -4,6 +4,10 @@ import Test.Hspec
 import Text.Printf (printf)
 
 main = hspec $ do
-    describe "canary" $ do
-        it "truthy" $ do
-           True `shouldBe` True
+  describe "canary" $ do
+    it "truthy" $ do
+      True `shouldBe` True
+
+  describe "apply a swap" $ do
+    it "swaps the balls" $ do
+      apply [0,1] [0,1,2] `shouldBe` [1,0,2]
