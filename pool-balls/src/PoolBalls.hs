ea27d6f088f1d5b302aa5ce2b78f4ab67c118d29
Alvaro Garcia <derecerca@gmail.com>
2016-12-10 18:46:48 +0000

    Green: use canned responsed

--- pool-balls/src/PoolBalls.hs
+++ pool-balls/src/PoolBalls.hs
@@ -3,6 +3,8 @@ module PoolBalls where
 import Data.Sequence (fromList, update)
 import Data.Foldable (toList)
 
+reorder current desired = [[0,1]]
+
 pairs :: Int -> [[Int]]
 pairs n = [[x,y] | y<-[0..n], x<-[0..y-1]]
 
