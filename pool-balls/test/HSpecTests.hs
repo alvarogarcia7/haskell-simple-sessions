bcde1be85083d4f8cf5a2bb546effa9af71a26d9
Alvaro Garcia <derecerca@gmail.com>
2016-12-10 18:45:56 +0000

    Red: problem 1+2

--- pool-balls/test/HSpecTests.hs
+++ pool-balls/test/HSpecTests.hs
@@ -11,3 +11,6 @@ main = hspec $ do
   describe "apply a swap" $ do
     it "swaps the balls" $ do
       apply [0,1] [0,1,2] `shouldBe` [1,0,2]
+
+    it "problem 1 - minimum number of swaps" $ do
+      reorder "YYR" "YRY" `shouldBe` [[0,1]]
