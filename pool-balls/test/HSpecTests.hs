4bc703b27975df60b2aa5a977dce0940b8df275c
Alvaro Garcia <derecerca@gmail.com>
2016-12-11 11:43:14 +0000

    Refactor: prefer using domain format

--- pool-balls/test/HSpecTests.hs
+++ pool-balls/test/HSpecTests.hs
@@ -10,7 +10,7 @@ main = hspec $ do
 
   describe "apply a swap" $ do
     it "swaps the balls" $ do
-      apply [0,1] [0,1,2] `shouldBe` [1,0,2]
+      apply [0,1] "012" `shouldBe` "102"
 
     it "problem 1 - minimum number of swaps" $ do
       reorder "ABC" "BAC" `shouldBe` [[0,1]]
