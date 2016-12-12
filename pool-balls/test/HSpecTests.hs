925a8bf8686751450f6c488b23e32b52c34e8416
Alvaro Garcia <derecerca@gmail.com>
2016-12-12 23:30:00 +0000

    WIP: add quickcheck to verify a property. Does not work, as it passes the tests

--- pool-balls/test/HSpecTests.hs
+++ pool-balls/test/HSpecTests.hs
@@ -1,19 +1,35 @@
 import PoolBalls
 
+import Debug.Trace
 import Test.Hspec
 import Text.Printf (printf)
 
-main = hspec $ do
-  describe "canary" $ do
-    it "truthy" $ do
-      True `shouldBe` True
+import Test.QuickCheck
+import System.Random
+import Data.Map
+ 
 
-  describe "apply a swap" $ do
-    it "swaps the balls" $ do
-      apply [0,1] "012" `shouldBe` "102"
+prop_halfWrongBalls :: String -> String -> Bool
+prop_halfWrongBalls balls b= False
+--  let shuffled = fisherYates (mkStdGen 0) balls
+--  let numberSwaps = length $ reorder balls (fst shuffled)
+--  let prop = ceiling $ toRational numberSwaps / 2
+--  let prop2 = fitnessFn balls
+  
 
-    it "problem 1 - minimum number of swaps" $ do
-      reorder "ABC" "BAC" `shouldBe` [[0,1]]
+fisherYatesStep :: RandomGen g => (Map Int a, g) -> (Int, a) -> (Map Int a, g)
+fisherYatesStep (m, gen) (i, x) = ((insert j x . insert i (m ! j)) m, gen')
+  where
+    (j, gen') = randomR (0, i) gen
+ 
+fisherYates :: RandomGen g => g -> [a] -> ([a], g)
+fisherYates gen [] = ([], gen)
+fisherYates gen l = 
+  toElems $ Prelude.foldl fisherYatesStep (initial (head l) gen) (numerate (tail l))
+  where
+    toElems (x, y) = (elems x, y)
+    numerate = zip [1..]
+    initial x gen = (singleton 0 x, gen)
 
-    it "problem 1 - minimum number of swaps, given multiple paths" $ do
-      reorder "ABA" "BAA" `shouldBe` [[0,1]]
+main = quickCheck prop_halfWrongBalls
+
