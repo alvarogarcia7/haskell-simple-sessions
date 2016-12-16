bdcffb4fb77da7ddeb38e7ff215a79d8f3722f8a
Alvaro Garcia <derecerca@gmail.com>
2016-12-16 20:15:37 +0000

    Refactor: recover example tests, put property testing in its file

--- pool-balls/test/HSpecTests.hs
+++ pool-balls/test/HSpecTests.hs
@@ -1,43 +1,22 @@
-module PoolsTest where
 
 import PoolBalls
 
-import Debug.Trace
 import Test.Hspec
 import Text.Printf (printf)
-
-import Test.QuickCheck
-import System.Random
-import Data.Map
  
+main = hspec $ do
+  describe "canary" $ do
+    it "truthy" $ do
+      True `shouldBe` True
 
-prop_halfWrongBalls :: String -> String -> Bool
-prop_halfWrongBalls balls b= do
-  let shuffled = fst $ fisherYates (mkStdGen 0) balls
-  let numberOfSwaps = length $ reorder balls shuffled
-  let wrongBalls = - (fitnessFn balls shuffled) :: Int
-  let halfWrongBalls = ceiling ((toRational wrongBalls) / (toRational 2))
-  let satisfiesProperty =  numberOfSwaps <= halfWrongBalls
-  let info = pprint [("original", balls), ("shuffled", shuffled), ("halfWrongBalls", show halfWrongBalls), ("swaps", show numberOfSwaps), ("applies", show satisfiesProperty)]
-  -- collect ((show numberSwaps)++", "++(show prop)) $ property (prop == prop2)
-  trace info satisfiesProperty 
+  describe "apply a swap" $ do
+    it "swaps the balls" $ do
+      apply [0,1] "012" `shouldBe` "102"
 
-pprint xs = unlines $ Prelude.map (\(a,b) -> a++(show b)) xs
-  
+    it "problem 1 - minimum number of swaps" $ do
+      reorder "ABC" "BAC" `shouldBe` [[0,1]]
 
-fisherYatesStep :: RandomGen g => (Map Int a, g) -> (Int, a) -> (Map Int a, g)
-fisherYatesStep (m, gen) (i, x) = ((insert j x . insert i (m ! j)) m, gen')
-  where
-    (j, gen') = randomR (0, i) gen
- 
-fisherYates :: RandomGen g => g -> [a] -> ([a], g)
-fisherYates gen [] = ([], gen)
-fisherYates gen l = 
-  toElems $ Prelude.foldl fisherYatesStep (initial (head l) gen) (numerate (tail l))
-  where
-    toElems (x, y) = (elems x, y)
-    numerate = zip [1..]
-    initial x gen = (singleton 0 x, gen)
+    it "problem 1 - minimum number of swaps, given multiple paths" $ do
+      reorder "ABA" "BAA" `shouldBe` [[0,1]]
 
-main = quickCheck prop_halfWrongBalls
 
---
+++ pool-balls/test/Properties.hs
@@ -0,0 +1,40 @@
+import PoolBalls
+
+import Debug.Trace
+import Text.Printf (printf)
+
+import Test.QuickCheck
+import System.Random
+import Data.Map
+ 
+
+prop_halfWrongBalls :: String -> String -> Bool
+prop_halfWrongBalls balls b= do
+  let shuffled = fst $ fisherYates (mkStdGen 0) balls
+  let numberOfSwaps = length $ reorder balls shuffled
+  let wrongBalls = - (fitnessFn balls shuffled) :: Int
+  let halfWrongBalls = ceiling ((toRational wrongBalls) / (toRational 2))
+  let satisfiesProperty =  numberOfSwaps <= halfWrongBalls
+  let info = pprint [("original", balls), ("shuffled", shuffled), ("halfWrongBalls", show halfWrongBalls), ("swaps", show numberOfSwaps), ("applies", show satisfiesProperty)]
+  -- collect ((show numberSwaps)++", "++(show prop)) $ property (prop == prop2)
+  trace info satisfiesProperty 
+
+pprint xs = unlines $ Prelude.map (\(a,b) -> a++(show b)) xs
+  
+
+fisherYatesStep :: RandomGen g => (Map Int a, g) -> (Int, a) -> (Map Int a, g)
+fisherYatesStep (m, gen) (i, x) = ((insert j x . insert i (m ! j)) m, gen')
+  where
+    (j, gen') = randomR (0, i) gen
+ 
+fisherYates :: RandomGen g => g -> [a] -> ([a], g)
+fisherYates gen [] = ([], gen)
+fisherYates gen l = 
+  toElems $ Prelude.foldl fisherYatesStep (initial (head l) gen) (numerate (tail l))
+  where
+    toElems (x, y) = (elems x, y)
+    numerate = zip [1..]
+    initial x gen = (singleton 0 x, gen)
+
+
+main = quickCheck prop_halfWrongBalls
