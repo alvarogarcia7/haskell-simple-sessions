e2d4a0eebc46488d5beb032dda4df6128db415df
Alvaro Garcia <derecerca@gmail.com>
2016-12-26 17:44:37 +0100

    Refactor: remove unnecessary parentheses

--- pool-balls/src/PoolBalls.hs
+++ pool-balls/src/PoolBalls.hs
@@ -29,7 +29,7 @@ findWorst current desired pastStates = do
   let notIn needle haystack = not $ any (==needle) haystack
   let notVisitedStates = filter (\(_, result) -> notIn result pastStates) swapAndItsResult
   let swapAndItsFitness = map (\(swap, newState) -> (swap, fitnessFn desired newState)) notVisitedStates :: [([Int], Int)]
-  let atLeast level = (level >= minFitnessLevel) where 
+  let atLeast level = level >= minFitnessLevel where 
       minFitnessLevel = fitnessFn desired current
   let acceptableSwaps = filter (\(_, fitness) -> atLeast fitness) swapAndItsFitness
   let minByFitness = (\first@(_, fitness) second@(_, fitness') -> if (fitness <= fitness') then first else second)
