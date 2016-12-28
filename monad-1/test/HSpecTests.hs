25323c326dd804882d4802d2dc8849126e4a005a
Alvaro Garcia <derecerca@gmail.com>
2016-12-28 19:40:59 +0000

    Green: add tests for current behaviour

--- monad-1/test/HSpecTests.hs
+++ monad-1/test/HSpecTests.hs
@@ -7,3 +7,12 @@ main = hspec $ do
   describe "canary" $ do
     it "truthy" $ do
       True `shouldBe` True
+
+  describe "applyting operations" $ do
+    it "And" $ do
+      apply (And True True) `shouldBe` True
+      apply (And True False) `shouldBe` False
+
+    it "Not" $ do
+      apply (Not True) `shouldBe` False
+      apply (Not False) `shouldBe` True
