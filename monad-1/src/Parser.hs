24c73bb73e86d800177dcd122e7ba2eaf90d8079
Alvaro Garcia <derecerca@gmail.com>
2016-12-28 20:04:02 +0000

    Red: cannot configure with a lambda
    [1 of 1] Compiling Parser           ( src/Parser.hs, dist/build/Parser.o )
    
    src/Parser.hs:3:53: error: parse error on input ‘\’
    Makefile:4: recipe for target 'test' failed

--- monad-1/src/Parser.hs
+++ monad-1/src/Parser.hs
@@ -1,6 +1,6 @@
 module Parser where
 
-data Operation a = And (Operation a) (Operation a) 
+data Operation a = And (Operation a) (Operation a) (\a b -> a && b)
                  | Not (Operation a)
                  | Expression a
   deriving (Show, Eq)
