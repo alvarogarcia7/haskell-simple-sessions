6afe6c11ccaf1c9b1605edecf5539603559c067c
Alvaro Garcia <derecerca@gmail.com>
2016-12-28 20:22:17 +0000

    Refactor: remove middleman

--- monad-1/src/Parser.hs
+++ monad-1/src/Parser.hs
@@ -1,19 +1,17 @@
 module Parser where
 
 class Oper a where
-  apply' :: Operation a -> a
+  apply :: Operation a -> a
   new :: a -> Operation a
 
 instance Oper Bool where
   new b = Expression b
-  apply' (And a b) = (apply' a) && (apply' b)
-  apply' (Not a) = not (apply' a)
-  apply' (Expression a) = a
+  apply (And a b) = (apply a) && (apply b)
+  apply (Not a) = not (apply a)
+  apply (Expression a) = a
 
 data Operation a = And (Operation a) (Operation a) 
                  | Not (Operation a)
                  | Expression a
   deriving (Show, Eq)
-
-apply b = apply' b
 
