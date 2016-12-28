db5290d6977a06d27ce032b64ede9bad35dec117
Alvaro Garcia <derecerca@gmail.com>
2016-12-28 20:06:24 +0000

    Create class Oper

--- monad-1/src/Parser.hs
+++ monad-1/src/Parser.hs
@@ -1,9 +1,14 @@
 module Parser where
 
+class Oper a where
+  apply' :: a -> a
+
 data Operation a = And (Operation a) (Operation a) 
                  | Not (Operation a)
                  | Expression a
   deriving (Show, Eq)
+
+
 
 apply :: Operation Bool -> Bool
 apply (And a b) = (apply a) && (apply b)
