26e820481cf42a8a82008d043d09b71d64830f84
Alvaro Garcia <derecerca@gmail.com>
2016-12-28 20:21:43 +0000

    Refactor: move the behaviour to instance Oper Bool

--- monad-1/src/Parser.hs
+++ monad-1/src/Parser.hs
@@ -1,16 +1,19 @@
 module Parser where
 
 class Oper a where
-  apply' :: a -> a
+  apply' :: Operation a -> a
+  new :: a -> Operation a
+
+instance Oper Bool where
+  new b = Expression b
+  apply' (And a b) = (apply' a) && (apply' b)
+  apply' (Not a) = not (apply' a)
+  apply' (Expression a) = a
 
 data Operation a = And (Operation a) (Operation a) 
                  | Not (Operation a)
                  | Expression a
   deriving (Show, Eq)
 
+apply b = apply' b
 
-
-apply :: Operation Bool -> Bool
-apply (And a b) = (apply a) && (apply b)
-apply (Not a) = not (apply a)
-apply (Expression a) = a
