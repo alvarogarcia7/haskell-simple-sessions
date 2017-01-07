3ce677c6f6425d1f88336cca5756a92676870389
Alvaro Garcia <derecerca@gmail.com>
2017-01-07 17:35:26 +0000

    Support boolean expressions

--- parser/src/boolean.hs
+++ parser/src/boolean.hs
@@ -4,25 +4,32 @@ import Text.Parsec.Token
 import Text.Parsec.Language (javaStyle)
 
 -- From http://stackoverflow.com/a/26539615/1181094
+-- modified to support boolean expressions
 
 lexer = makeTokenParser javaStyle
 
 expr    = buildExpressionParser table term
         <?> "expression"
 
-term    =  parens lexer expr 
-        <|> natural lexer
+term    = identifier lexer 
         <?> "simple expression"
 
-table   = [ [prefix "-" negate, prefix "+" id ]
-        , [postfix "++" (+1)]
-        , [binary "*" (*) AssocLeft, binary "/" (div) AssocLeft ]
-        , [binary "+" (+) AssocLeft, binary "-" (-)   AssocLeft ]
+table   = [ [prefix "NOT" not_]
+        , [binary "AND" (and_) AssocLeft]
+        , [binary "OR" (or_) AssocLeft]
         ]
 
 binary  name fun assoc = Infix (do{ reservedOp lexer name; return fun }) assoc
 prefix  name fun       = Prefix (do{ reservedOp lexer name; return fun })
-postfix name fun       = Postfix (do{ reservedOp lexer name; return fun })
 
-parseExpression :: String -> Either ParseError Integer
+parseExpression :: String -> Either ParseError String
 parseExpression expression = parse expr "(unknown)" expression
+
+not_ "T" = "F" 
+not_ _ = "T"
+
+and_ "T" "T" = "T"
+and_ _ _ = "F"
+
+or_ "F" "F" = "F"
+or_ _ _ = "T"
