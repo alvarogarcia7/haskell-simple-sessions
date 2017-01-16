ea35b75bf27401b0c6e9cdcf7159312184efbeae
Alvaro Garcia <derecerca@gmail.com>
2017-01-16 19:22:00 +0000

    Refactor: remove redundant parenthesis

--- parser/test/booleanTests.hs
+++ parser/test/booleanTests.hs
@@ -11,32 +11,32 @@ main = hspec $ do
   describe "parse expressions" $ do
     describe "simple expressions" $ do  
       it "parses T" $ do
-        parseExpression "T" `shouldBe` (Right "T")
+        parseExpression "T" `shouldBe` Right "T"
 
       it "parses F" $ do
-        parseExpression "F" `shouldBe` (Right "F")
+        parseExpression "F" `shouldBe` Right "F"
 
     describe "negated expressions" $ do  
       it "case 1" $ do
-        parseExpression "NOT T" `shouldBe` (Right "F")
-        parseExpression "NOT F" `shouldBe` (Right "T")
+        parseExpression "NOT T" `shouldBe` Right "F"
+        parseExpression "NOT F" `shouldBe` Right "T"
 
     describe "operation AND" $ do  
       it "case 1" $ do
-        parseExpression "T AND T" `shouldBe` (Right "T")
+        parseExpression "T AND T" `shouldBe` Right "T"
 
       it "supports multiple expressions" $ do
-        parseExpression "T AND T AND T" `shouldBe` (Right "T")
-        parseExpression "T AND T AND F" `shouldBe` (Right "F")
+        parseExpression "T AND T AND T" `shouldBe` Right "T"
+        parseExpression "T AND T AND F" `shouldBe` Right "F"
 
     describe "operation OR" $ do  
       it "case 1" $ do
-        parseExpression "F OR F" `shouldBe` (Right "F")
+        parseExpression "F OR F" `shouldBe` Right "F"
 
     describe "combining multiple operations" $ do
       it "AND and NOT" $ do
-        parseExpression "T AND NOT F" `shouldBe` (Right "T")
+        parseExpression "T AND NOT F" `shouldBe` Right "T"
 
 --      it "NOT and NOT" $ do
---        parseExpression "NOT NOT F" `shouldBe` (Right "F")
+--        parseExpression "NOT NOT F" `shouldBe` Right "F"
 
