914d4a63da8524b2e87617f95272f421cb7491c2
Alvaro Garcia <derecerca@gmail.com>
2017-01-19 01:45:24 +0000

    Red: empty board

--- tictactoe/test/HSpecTests.hs
+++ tictactoe/test/HSpecTests.hs
@@ -7,3 +7,8 @@ main = hspec $ do
   describe "canary" $ do
     it "truthy" $ do
        True `shouldBe` True
+
+  describe "tic tac toe" $ do
+    it "should be an empty board to start with" $ do
+       game `shouldBe` [[' ',' ',' '], [' ',' ',' '], [' ',' ',' ']]
+
