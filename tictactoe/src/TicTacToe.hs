f409d9944ad25fcec5825204ef3eca52f9fc39a9
Alvaro Garcia <derecerca@gmail.com>
2017-01-19 01:52:17 +0000

    green

--- tictactoe/src/TicTacToe.hs
+++ tictactoe/src/TicTacToe.hs
@@ -1,12 +1,13 @@
 module TicTacToe where
 
 type Board = [[Maybe Char]]
-data Game = Game { board::Board }
+data Game = Game { board::Board, currentPlayer::Char }
 
 game :: Game
 game = Game {
         board=[[Nothing, Nothing, Nothing],
                [Nothing, Nothing, Nothing],
-               [Nothing, Nothing, Nothing]]
+               [Nothing, Nothing, Nothing]],
+        currentPlayer='X'
        }
 
