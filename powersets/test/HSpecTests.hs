9fb18fc557e2cba20e24a4e9c0a4c0db1e02573d
Alvaro Garcia <derecerca@gmail.com>
2017-01-19 20:25:32 +0000

    Refactor: adapt session to cabal

--- powersets/test/HSpecTests.hs
+++ powersets/test/HSpecTests.hs
@@ -1,27 +1,5 @@
-module PowerSets where
-
+import PowerSets
 import Test.Hspec
-import Data.List
-import Text.Printf (printf)
-
-powersets :: [x] -> [[x]]
-powersets xs = sortBy size $ powersets' xs where
-    powersets' xs = flatmap addHeadTo (pieces xs) where
-        addHeadTo [] = [[]]
-        addHeadTo (x':xs') = map (x':) (powersets' xs')
-
--- Pieces: generates immediate results of removing elements from the left, one by one
--- See tests
-pieces :: [x] -> [[x]]
-pieces = tails
-
-size a b | length a > length b = GT
-         | length a < length b = LT
-         | length a == length b = EQ
-
-
-flatmap :: (x -> [x]) -> [x] -> [x]
-flatmap f xs = foldl (\acc ele-> acc ++ (f ele)) [] xs
 
 main = hspec $ do
         describe "canary" $ do
